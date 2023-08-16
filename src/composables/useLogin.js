bb336bc1f7192e1e16376b1bb6755cb39b74575e
Yazid <yazeedkurdi@hotmail.com>
2023-08-16 17:20:00 +0300

    login composable

---
+++ src/composables/useLogin.js
@@ -0,0 +1,29 @@
+// src/composables/useLogin.js
+import { useAuthStore } from '@/store/index.js';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+export default function useLogin() {
+  const authStore = useAuthStore();
+  const router = useRouter();
+
+  const login = async (username, password) => 
+  {
+    axios.defaults.headers.common['Authorization'] = '';
+    localStorage.removeItem('token');
+
+    try {
+      const response = await axios.post('/dj-rest-auth/login/', {
+        username: username,
+        password: password,
+      });
+      const token = response.data.key;
+      authStore.setToken(token);
+      router.push({ name: 'Landing' }); // Redirect after successful login
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
+
+  return { login };
+}
