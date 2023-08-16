8e42091d6fb6e2a665287e7276e3f40775a32727
Yazid <yazeedkurdi@hotmail.com>
2023-08-16 12:33:07 +0300

    primary color

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+import ChildComponent from './components/Header.vue'
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <ChildComponent />
+  </div>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; // Import the path module
 
-// https://vitejs.dev/config/
+// Add this function to generate the alias
+function pathResolve(dir) {
+  return path.resolve(__dirname, '.', dir);
+}
+
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': pathResolve('src'), // This creates an alias for the "src" directory
+    },
+  },
+});
