28e031376e52ace8f63beb6e0607f968552c5287
Yazid <yazeedkurdi@hotmail.com>
2023-08-16 17:20:34 +0300

    init configurations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+const pinia = createPinia()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
+
