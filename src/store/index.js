c0192638d0a629e27553a255d2df465debd1f3d2
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 11:18:59 +0300

    initialize store function used on reload

--- src/store/index.js
+++ src/store/index.js
@@ -2,20 +2,27 @@
 import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    token: localStorage.getItem('token') || null,
-  }),
-  getters: {
-    isAuthenticated: (state) => !!state.token,
-  },
-  actions: {
-    setToken(token) {
-      this.token = token;
-      localStorage.setItem('token', token);
+    state: () => ({
+        token: localStorage.getItem('token') || null
+    }),
+    getters: {
+        isAuthenticated: (state) => !!state.token,
     },
-    clearToken() {
-      this.token = null;
-      localStorage.removeItem('token');
+    actions: {
+        setToken(token) {
+            this.token = token
+            localStorage.setItem('token', token)
+        },
+        clearToken() {
+            this.token = null
+            localStorage.removeItem('token')
+        },
+        initializeStore() {
+            if (this.isAuthenticated) {
+                this.token = localStorage.getItem('token')
+            } else {
+                this.token = ''
+            }
+        }
     },
-  },
 });
