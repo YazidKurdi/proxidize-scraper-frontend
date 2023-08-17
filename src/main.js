8a77b9b2dfec7ea14a33118908615309b11637e6
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 23:06:42 +0300

    vite typo

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import axios from 'axios'
 import Notifications from 'notiwind'
 
-axios.defaults.baseURL = import.meta.env.BACKEND_API || 'http://127.0.0.1:8000'
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API || 'http://127.0.0.1:8000'
 
 const pinia = createPinia()
 const app = createApp(App)
