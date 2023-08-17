c92e9cddc9cdfcb6c20b2e3b118a162acbcf977c
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 18:50:58 +0300

    init notifications

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './style.css'
 import App from './App.vue'
 import axios from 'axios'
+import Notifications from 'notiwind'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
@@ -11,5 +12,6 @@ const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(Notifications)
 app.mount('#app')
 
