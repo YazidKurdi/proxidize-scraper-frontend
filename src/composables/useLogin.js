bb3f32ed7eb99e77231cc8df409a6ed7faedd577
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 11:20:18 +0300

    refactor & add auth header

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -1,16 +1,16 @@
 // src/composables/useLogin.js
-import { useAuthStore } from '@/store/index.js';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
+import { useAuthStore } from '@/store/index.js'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
 
 export default function useLogin() {
-  const authStore = useAuthStore();
-  const router = useRouter();
+  const authStore = useAuthStore()
+  const router = useRouter()
 
   const login = async (username, password) => 
   {
-    axios.defaults.headers.common['Authorization'] = '';
-    localStorage.removeItem('token');
+    axios.defaults.headers.common['Authorization'] = ''
+    localStorage.removeItem('token')
 
     try {
       const response = await axios.post('/dj-rest-auth/login/', {
@@ -18,12 +18,14 @@ export default function useLogin() {
         password: password,
       });
       const token = response.data.key;
-      authStore.setToken(token);
-      router.push({ name: 'Landing' }); // Redirect after successful login
+      authStore.setToken(token)
+      axios.defaults.headers.common["Authorization"] = "Token " + token
+
+      router.push({ name: 'Table' }); // Redirect after successful login
     } catch (error) {
-      console.error('Login error:', error);
+      console.error('Login error:', error)
     }
   };
 
-  return { login };
+  return { login }
 }
