64171261c6dd0104889cdbe5a9131d9ea6d1aa77
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 23:57:59 +0300

    backend

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import axios from 'axios'
 import Notifications from 'notiwind'
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API || 'api'
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API || 'https://squid-app-ilmf2.ondigitalocean.app/'
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
