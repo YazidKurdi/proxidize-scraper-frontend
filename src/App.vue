34818d044bf9c3b3f04404524620ea5616c7ea43
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 11:21:53 +0300

    implement init store and token headers

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-    <Navbar />
-    <div class="content">
+  <Navbar />
+  <div class="bg-gray-50 h-[91vh]">
+    <div class="flex flex-col items-center px-6 py-8 mx-auto lg:py-0">
       <router-view />
     </div>
+  </div>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
+import { useAuthStore } from '@/store/index.js'
+import axios from 'axios'
+
+
+const authStore = useAuthStore()
+
+authStore.initializeStore()
+
+const token = authStore.token
+
+if (token) {
+  axios.defaults.headers.common['Authorization'] = "Token " + token
+} else {
+  axios.defaults.headers.common['Authorization'] = ""
+}
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
